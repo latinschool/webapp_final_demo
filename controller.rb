2f838e2f1a5a37143c93c12a647dbb9993ed05cb
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-05-18 11:06:20 -0500

    user setup did

--- controller.rb
+++ controller.rb
@@ -8,33 +8,3 @@ require './models/all.rb'
 get '/' do
 	erb :index
 end
-
-post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-        redirect "/login"
-    else
-        redirect "/failure"
-    end
-end
-
-
-post "/login" do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect "/success"
-    else
-        redirect "/failure"
-    end
-end
-
-
-get '/success' do
-	erb :index
-end
-
-
-get '/failure' do
-	erb :index
-end
